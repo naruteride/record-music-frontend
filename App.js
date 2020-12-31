b1003d1c3e32a92eb66e3fc284dde53c55221c3f
bangseonghun <slimejam01@gmail.com>
2020-12-31 16:48:43 +0900

    푸터 틀 잡음

--- App.js
+++ App.js
@@ -2,11 +2,28 @@ import React from 'react';
 import { View, Text, Image, Button, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// 하단 탭에 들어갈 컴포넌트들
 import MainScreen from './components/MainScreen.js';
+import HomeStackScreen from "./components/AppTabNavigator/HomeTab.js";
 
+
+// App()
+export default function App() {
+	return (
+		<>
+			< NavigationContainer >
+				<MyTabs />
+			</NavigationContainer >
+		</>
+	);
+};
+
+
+
+// 해더
 function HeaderBar() {
-
-
 	return (
 
 		<View style={{
@@ -30,30 +47,62 @@ function HeaderBar() {
 	);
 }
 
-const Stack = createStackNavigator();
 
-function RootStack() {
+
+
+// 하단 네비게이션
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
 	return (
-		<Stack.Navigator
-			initialRouteName="Home"
-			screenOptions={{ gestureEnabled: false }}
-		>
-			<Stack.Screen
-				name="Home"
-				component={MainScreen}
-				options={{
-					headerTitle: props => <HeaderBar {...props} />,
-				}}
-			/>
-		</Stack.Navigator>
+		<Tab.Navigator>
+			<Tab.Screen name="Main" component={MainScreen} />
+			<Tab.Screen name="Home" component={HomeStackScreen} />
+			<Tab.Screen name="Settings" component={SettingsStackScreen} />
+		</Tab.Navigator>
 	);
 }
 
-export default function App() {
-	return <NavigationContainer>
-		<RootStack />
-	</NavigationContainer>;
+
+// 셋팅
+const SettingsStack = createStackNavigator();
+function SettingsStackScreen() {
+	return (
+		<SettingsStack.Navigator>
+			<SettingsStack.Screen name="Settings" component={SettingsScreen} />
+			<SettingsStack.Screen name="Details" component={DetailsScreen} />
+		</SettingsStack.Navigator>
+	);
 }
+
+
+
+function SettingsScreen({ navigation }) {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Settings screen</Text>
+			<Button
+				title="Go to Details"
+				onPress={() => navigation.navigate('Details')}
+			/>
+		</View>
+	);
+}
+
+
+function DetailsScreen() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Details!</Text>
+		</View>
+	);
+}
+
+
+
+
+
+
 
 const styles = {
 
--- components/AppTabNavigator/HomeTab.js
+++ components/AppTabNavigator/HomeTab.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, StyleSheet, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+// import { SearchBar } from 'react-native-elements';   // 검색바 관련 모듈
 
-function HomeTab() {
-    return (
-        <View style={style.container}>
-            <Text>HomeTab</Text>
-        </View>
-    );
+
+const HomeStack = createStackNavigator();
+function HomeStackScreen() {
+	return (
+		<HomeStack.Navigator>
+			<HomeStack.Screen name="Home" component={HomeScreen} />
+			<HomeStack.Screen name="Details" component={DetailsScreen} />
+		</HomeStack.Navigator>
+	);
 }
 
-const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+
+
+function HomeScreen({ navigation }) {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Home screen</Text>
+			<Button
+				title="Go to Details"
+				onPress={() => navigation.navigate('Details')}
+			/>
+		</View>
+	);
+}
+
+
+function DetailsScreen() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Details!</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+
 });
+
+export default HomeStackScreen;
